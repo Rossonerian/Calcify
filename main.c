156364f114db32dba466558ddc48640a14f84146
Hardhik bhatia <hardhikbhatia0@gmail.com>
2025-10-31 22:11:26 +0530

    git commit
    Wrote code for basic input

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//For Initial Purpose,We are taking type of operation and values separately (May Change Later)
+
+
+
+void main()
+{
+    float value1,value2;
+    char operant;
+    float result;
+
+
+    printf("Enter Value 1 and 2:- ");
+    scanf("Value 1 :- %f",value1);
+    scanf("Value 2 :- %f",value2);
+    scanf("Operator:- %c");
+
+    
+}
