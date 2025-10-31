f9a4a7e82a427e8d0df2bce6ac2c8fa3b030bead
Hardhik bhatia <hardhikbhatia0@gmail.com>
2025-10-31 22:52:24 +0530

    git commit

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "Calc.c"
+
 //For Initial Purpose,We are taking type of operation and values separately (May Change Later)
 
 
@@ -9,40 +10,56 @@ void main()
     float value1,value2;
     char operant;
     double result;
+    char opt;
 
 
     printf("Enter Value 1 and 2(Second Value can be zero for square,square root and reciprocal):- \n");
     scanf("%f %f",&value1,&value2);
-    printf("Enter Operant(+,-,*,/,^(square),?(square root),~(reciprocal)):- ");
+    printf("Enter Operant(+,-,*,/,^(square),?(square root),~(reciprocal)):- \n");
     scanf(" %c",&operant);
 
-    switch (operant)
-    {
-    case '+':
-        result = add(value1,value2);
-        break;
-    case '-':
-        result = sub(value1,value2);
-        break;
-    case '*':
-        result = mul(value1,value2);
-        break;
-    case '/':
-        result = div(value1,value2);
-        break;
-    case '^':
-        result = sqr(value1);
-        break;
-    case '?':
-        result = sqroot(value1);
-        break;
-    case '~':
-        result = inv(value1);
-        break;
-    default:
-        break;
-    }
+    calc:
+        switch (operant)
+        {
+        case '+':
+            result = add(value1,value2);
+            break;
+        case '-':
+            result = sub(value1,value2);
+            break;
+        case '*':
+            result = mul(value1,value2);
+            break;
+        case '/':
+            result = div(value1,value2);
+            break;
+        case '^':
+            result = sqr(value1);
+            break;
+        case '?':
+            result = sqroot(value1);
+            break;
+        case '~':
+            result = inv(value1);
+            break;
+        default:
+            break;
+        }
+        
+        printf("Result = %f\n",result);
 
-    printf("Result = %f",result);
+        printf("Do you wish to continue(Y/N):- \n");
+        scanf(" %c",&opt);
 
+        while ((opt-32) == 'Y' || opt == 'Y' )
+        {
+            value2=0;
+            printf("Enter Value:- \n");
+            scanf("%f",&value2);
+            printf("Enter Operant(+,-,*,/,^(square),?(square root),~(reciprocal)):- \n");
+            scanf(" %c",&operant);
+
+            value1 = result;
+            goto calc;
+        }
 }
