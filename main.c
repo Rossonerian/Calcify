db322665cdcce998c4e7951901b8ea6b9a7d296a
Hardhik bhatia <hardhikbhatia0@gmail.com>
2025-10-31 22:32:14 +0530

    Update main.c

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ void main()
     printf("Enter Value 1 and 2(Second Value can be zero for square,square root and reciprocal):- \n");
     scanf("%f",&value1);
     scanf("%f",&value2);
-    printf("Enter Operant(+,-,*,/,^(square),?(square root),~(reciprocal)):- ")
+    printf("Enter Operant(+,-,*,/,^(square),?(square root),~(reciprocal)):- ");
     scanf("%c",&operant);
 
     switch (operant)
