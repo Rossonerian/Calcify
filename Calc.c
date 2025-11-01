7e4abef2ee7e115310cb7f02c5bfb017ce5ee949
Divyansh Rai <raidiv.3608@gmail.com>
2025-11-01 16:07:27 +0530

    Create Calc.c

---
+++ Calc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+//assuming only 2 inputs are entered initially
+extern float add(float a,float b)
+{
+    return (a+b);
+}
+extern float sub(float a,float b)
+{
+    float d=a-b;
+    return d;
+}
+extern float mul(float a,float b)
+{
+    return (a*b);
+}
+extern float div(float a,float b)
+{
+    return (a/b);
+}
+extern float sqr(float a)
+{
+    return (a*a);
+}
+extern double sqroot(float a)
+{   double d=sqrt(a);
+    return d;
+}
+extern double inv(float a)
+{
+    return (1/a);
+}
